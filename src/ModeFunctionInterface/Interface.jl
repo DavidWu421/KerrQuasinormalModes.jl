bf85d64bf68a54b37b424aab5fe340d59896fd12
David Wu <dwuuu.421@gmail.com>
2024-08-21 13:57:56 -0400

    Add arugment conjugation to minus radial modes

--- src/ModeFunctionInterface/Interface.jl
+++ src/ModeFunctionInterface/Interface.jl
@@ -160,10 +160,10 @@ function qnmfunction(::typeof(Custom); s=-2,l=2,m=2,n=0,a=0.00, ω = Complex(0.0
     QuasinormalModeFunction(s,l,m,n,a,ω,Alm,Ψᵣ,Ψᵪ)
 end
 
-(Ψ::QuasinormalModeFunction)(r; isconjugate=false) = Ψ.R(r;isconjugate=isconjugate) 
-(Ψ::QuasinormalModeFunction)(r, z; isconjugate=false) = Ψ.R(r;isconjugate=isconjugate) * Ψ.S(z;isconjugate=isconjugate)
-(Ψ::QuasinormalModeFunction)(r, z, ϕ) =  Ψ.R(r)*Ψ.S(z)*exp(im*Ψ.m*ϕ)
-(Ψ::QuasinormalModeFunction)(r, z, ϕ, t) =  Ψ.R(r)*Ψ.S(z)*exp(im*Ψ.m*ϕ)*exp(-im*Ψ.ω*t)
+(Ψ::QuasinormalModeFunction)(r; isconjugate=false) = real(Ψ.ω) < 0 ? Ψ.R(conj(r); isconjugate=isconjugate) : Ψ.R(r; isconjugate=isconjugate)
+(Ψ::QuasinormalModeFunction)(r, z; isconjugate=false) = real(Ψ.ω) < 0 ? Ψ.R(conj(r);isconjugate=isconjugate) * Ψ.S(z;isconjugate=isconjugate) : Ψ.R(r;isconjugate=isconjugate) * Ψ.S(z;isconjugate=isconjugate)
+(Ψ::QuasinormalModeFunction)(r, z, ϕ) =  real(Ψ.ω) < 0 ?  Ψ.R(conj(r))*Ψ.S(z)*exp(im*Ψ.m*ϕ) : Ψ.R(r)*Ψ.S(z)*exp(im*Ψ.m*ϕ)
+(Ψ::QuasinormalModeFunction)(r, z, ϕ, t) =  real(Ψ.ω) < 0 ? Ψ.R(conj(r))*Ψ.S(z)*exp(im*Ψ.m*ϕ)*exp(-im*Ψ.ω*t) : Ψ.R(r)*Ψ.S(z)*exp(im*Ψ.m*ϕ)*exp(-im*Ψ.ω*t)
 
 (Ψ::QuasinormalModeFunction)(x::NamedTuple{(:r,),Tuple{Number}}) = Ψ.R(x[:r])
 (Ψ::QuasinormalModeFunction)(x::NamedTuple{(:θ,),Tuple{Number}}) = Ψ.S(cos(x[:θ]))
